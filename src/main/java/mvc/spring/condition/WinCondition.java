fd0d0dc2754cc944bcf35ed931741ee6e94e9c42
hymanlew <wohena3689>
2020-08-30 12:00:27 +0800

    update

--- src/main/java/mvc/spring/condition/WinCondition.java
+++ src/main/java/mvc/spring/condition/WinCondition.java
@@ -9,7 +9,13 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 
 /**
  * 自定义 condition
- * matches 方法指定了一个条件匹配规则，返回 true 则注入所标注的 Bean 类到 IOC 容器，反之则不注入。
+ *
+ * @Conditional 注解作用：
+ * 必须是 @Conditional 指定的条件成立，才给容器中添加组件，配置配里面的所有内容才生效；matches 方法指定了一个条件匹配规则，返回 true
+ * 则注入所标注的 Bean 类到 IOC 容器，反之则不注入。即自动配置类必须在一定的条件下才能生效。
+ *
+ * 怎么知道哪些自动配置类生效；可以在主配置文件中启用 debug=true 属性；来让控制台打印自动配置报告进行查看。
+ * Positive matches：代表自动配置成功，Negative matches：没有自动配置，在有需要时再导入包，再查看。
  */
 public class WinCondition implements Condition {
 
